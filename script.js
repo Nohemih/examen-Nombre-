b9a588c351e4c9fcb34ae7a492d773178be36b02
Nohemih <nohemih800@gmail.com>
2017-03-10 12:28:31 -0600

    examen

---
+++ script.js
@@ -0,0 +1,8 @@
+
+var nohemi = angular.module('nohemi', []);
+nohemi.controller('operacion', function($scope) {
+$scope.cantidad1= 8000
+$scope.cantidad2= 0.12
+$scope.cantidad1 = $scope.cantidad1
+  $scope.cantidad1 = $scope.cantidad2 * $scope.cantidad1;
+});
